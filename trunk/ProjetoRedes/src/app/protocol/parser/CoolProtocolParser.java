1369f85a19e2c80e23ea1cb6a494905822b61bd4
thiagoponte@gmail.com <thiagoponte@gmail.com@de264584-aa5c-c072-3368-09cac1cf7177>
2012-09-11 01:26:06 +0000

    aula segunda

---
+++ trunk/ProjetoRedes/src/app/protocol/parser/CoolProtocolParser.java
@@ -0,0 +1,28 @@
+package app.protocol.parser;
+
+import sun.misc.CRC16;
+import app.protocol.Frame;
+
+public class CoolProtocolParser {
+
+	public static Frame parseTo(byte[] destination, byte[] source, byte groupID, byte[] payload) {
+
+		// do the magic to create a frame packet
+		Frame parsed = new Frame(destination, source, groupID, payload);
+		parsed.setMessageID((byte) 0x01);
+		parsed.setPayloadLength(new Integer(payload.length).byteValue());
+//		parsed.setCrc16()
+        // TODO - criar na classe frame o metodo que devolve o conteudo concatenado em um so array de byte
+		
+		return parsed;
+	}
+
+	public static String parseFrom(byte[] message) {
+
+		// gera string baseada no byte array
+		
+		
+		return "";
+	}
+
+}
