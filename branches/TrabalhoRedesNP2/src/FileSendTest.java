639848efd774786c999d4279bd323662e4b4d67a
thiagoponte@gmail.com <thiagoponte@gmail.com@de264584-aa5c-c072-3368-09cac1cf7177>
2012-11-25 22:39:32 +0000

    yes

--- branches/TrabalhoRedesNP2/src/FileSendTest.java
+++ branches/TrabalhoRedesNP2/src/FileSendTest.java
@@ -1,30 +1,47 @@
+
+import infra.HeaderUtils;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.zip.CRC32;
-import java.util.zip.CheckedInputStream;
+
+import sun.misc.IOUtils;
+import util.ArrayUtil;
+import util.CRC16;
 
 public class FileSendTest {
 	public static void enviarArquivo(File fileToSend) {
 		Socket socket = null;
-		FileInputStream in = null;
+		ByteArrayInputStream in = null;
 		OutputStream out = null;
 		try {
 			if (fileToSend.canRead()) {
-				socket = new Socket("127.0.0.1",Server.PORT);
-				long crc = calcularCrc(fileToSend);
-				System.out.println("Client CRC: "+crc);
+				socket = new Socket("localhost",Server.PORT);
+//				long crc = calcuarCrc(fileToSend);
+//				System.out.println("Client CRC: "+crc);
 				out = socket.getOutputStream();
+				System.out.println("Integer: "+(int)fileToSend.length());
+				System.out.println("Long: "+fileToSend.length());
+				byte[] header = new HeaderUtils(fileToSend.getName(), (int)fileToSend.length()).getHeader();
 				
-				in = new FileInputStream(fileToSend);
+//				String calculate = CRC16.calculate(fileToSend);
+				byte[] fullContent = ArrayUtil.concat(header, IOUtils.readFully(new FileInputStream(fileToSend), (int) fileToSend.length(), true));
 				
+				String calculate = CRC16.calculate(fullContent);
+//				CRC16.addCRC(fullContent,calculate);
+				System.out.println("CRC-16 READ FROM CLIENT: "+calculate);
+				
+//				in = new FileInputStream(fileToSend);
+				in = new ByteArrayInputStream(fullContent);
 				byte[] buffer = new byte[Server.BUFFER_SIZE];
+
 				while (( in.read(buffer) )!= -1) {
 					out.write(buffer);
 					out.flush();
 				}
+				
 				out.close();
 				in.close();
 			}
@@ -45,13 +62,4 @@ public class FileSendTest {
 		}
 	}
 	
-	private static long calcularCrc(File file) throws IOException {
-		CheckedInputStream cin = new CheckedInputStream(new FileInputStream(file), new CRC32());
-		byte[] buffer = new byte[Server.BUFFER_SIZE];
-		while(cin.read(buffer) != -1){
-			//utilizado para ler o arquivo
-		}
-		cin.close();
-		return cin.getChecksum().getValue();
-	}
 }
